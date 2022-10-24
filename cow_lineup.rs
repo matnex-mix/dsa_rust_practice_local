87816fdbb34857bca36c6b128e22bf386cca2ff9
Matnex Mix <jolaoshobatmat@gmail.com>
2022-10-24 17:59:10 +0100

    eventually completed the cow lineup which i'm sure isn't O(n) i don't think it's even possible to do strictly O(n)

--- cow_lineup.rs
+++ cow_lineup.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashSet, HashMap};
 
 fn main() {
 
@@ -5,8 +6,67 @@ fn main() {
 
 }
 
-fn solution( lineup: Vec<(i32, i32) ) -> i32 {
+fn solution( mut lineup: Vec<(i32, i32)> ) -> i32 {
 
-	lineup.sort_by(|a, b| a.1.cmp(&b.1));
+	let mut breeds = HashSet::new();
+	let mut checked = HashMap::new();
 
+	let mut count = i32::MAX;
+	let mut start = 0;
+
+	lineup.sort_by(|a, b| {
+		breeds.insert(a.1);
+		breeds.insert(b.1);
+
+		a.0.cmp(&b.0)
+	});
+
+	let breed_count = breeds.len();
+
+	breeds.clear();
+
+	for end in 0..lineup.len() {
+		//println!("AT POINT {}", end);
+		checked.entry(lineup[end].1).and_modify(|c| *c += 1).or_insert(1);
+		//println!("{:?}", checked);
+
+		// if it is not good, increase the end
+		if checked.len() != breed_count {
+			continue;
+		}
+		// if it is good then increase start
+		else {
+			//println!("start increasing the start pointer");
+			while checked.len() == breed_count && start < end {
+				match checked.get_mut(&lineup[end].1) {
+					Some(c) => {
+						if c > &mut 1 {
+							*c -= 1;
+							start += 1;
+						}
+						else {
+
+							//println!("end {}, start {}", end, start);
+
+							let _count = lineup[end].0 - lineup[start].0;
+							if _count < count {
+								count = _count;
+							}
+
+							break;
+						}
+					},
+					None => {
+						break;
+					},
+				}
+
+				//println!("{}", start);
+				//println!("{:?}", checked);
+			}
+		}
+	}
+
+	//println!("result: {}", count);
+	count as i32
 }
